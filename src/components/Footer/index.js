276abbc961009461a1d33fbd7f2b57e698743d46
CassBut <clhurlbut@gmail.com>
2021-03-14 11:04:48 -0700

    trying to install iconify

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,8 +2,13 @@ import React from "react"
 
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
+
+        <div className="footer" >
+          Made with love!
+          
+           {/*  <a href="https://github.com/clhurlbut" target="_blank" rel="noreferrer" id="footerLink"><span className="iconify" data-icon="ant-design:github-outlined" data-inline="false"></span></a>
+            <a href="https://www.linkedin.com/in/clhurlbut/" target="_blank" rel="noreferrer" id="footerLink"><span className="iconify" data-icon="openmoji:linkedin" data-inline="false"></span></a>
+            <a href="https://www.instagram.com/cassbut/" target="_blank" rel="noreferrer" id="footerLink"><span className="iconify" data-icon="openmoji:instagram" data-inline="false"></span></a> */}
         </div>
     )
 }
