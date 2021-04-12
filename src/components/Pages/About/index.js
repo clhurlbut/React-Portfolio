c45b4132930148626913dae6a7d2b5002ceabd98
CassBut <clhurlbut@gmail.com>
2021-04-11 18:28:30 -0700

    fixed spacing on about page

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -18,11 +18,7 @@ function AboutMe() {
                         Immediately after graduation I worked for Warner Bros on a hit television show. After I left the show, I became certified as a
                         therapeutic riding instructor with the Professional Association of Therapeutic Horsemanship, INTL, and as an Equine Specialist with
                         the Equine Assisted Growth And Learning Association.
-                        
-                        </Col>
-            </Row>
-            <Row>
-                <Col>
+           
                         I have worked with a non-profit for the last several years, and decided I needed to make another change. I have been
                         enjoying coding and will be updating this section as class progresses. I want to use my new skill set with web development
                         to help better the lives of those with different abilities.
