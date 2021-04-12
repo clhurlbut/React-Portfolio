40098e1bb330e0b3dea1c317e7230e0e7bcefbce
CassBut <clhurlbut@gmail.com>
2021-04-11 18:33:50 -0700

    one last style to profile before mvp pre styling

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -13,16 +13,18 @@ function AboutMe() {
                     <Image src={ProfilePhoto} alt="Cassie on the beach with a hat" fluid />
                 </Col>
                 <Col>
-                    
-                        I graduated from Columbia College Hollywood with a BFA in Cinema Producing and Screenwriting.
-                        Immediately after graduation I worked for Warner Bros on a hit television show. After I left the show, I became certified as a
-                        therapeutic riding instructor with the Professional Association of Therapeutic Horsemanship, INTL, and as an Equine Specialist with
-                        the Equine Assisted Growth And Learning Association.
-           
+                    <div fluid>      I graduated from Columbia College Hollywood with a BFA in Cinema Producing and Screenwriting.
+                    Immediately after graduation I worked for Warner Bros on a hit television show. After I left the show, I became certified as a
+                    therapeutic riding instructor with the Professional Association of Therapeutic Horsemanship, INTL, and as an Equine Specialist with
+                    the Equine Assisted Growth And Learning Association.
+                    </div>
+                    <br></br>
+                    <div fluid>
                         I have worked with a non-profit for the last several years, and decided I needed to make another change. I have been
                         enjoying coding and will be updating this section as class progresses. I want to use my new skill set with web development
                         to help better the lives of those with different abilities.
-          
+                        </div>
+
                 </Col>
             </Row>
         </Container>
